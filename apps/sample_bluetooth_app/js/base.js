0294963f8b0857e82417164de7dee173a35869e7
Soumya Kethu <skethu@andrew.cmu.edu>
2014-03-12 17:29:51 -0400

    made more changes to base

--- apps/sample_bluetooth_app/js/base.js
+++ apps/sample_bluetooth_app/js/base.js
@@ -1,59 +1,64 @@
+/* global alert, console */
+'use strict';
 // Install app
 if (navigator.mozApps) {
     var checkIfInstalled = navigator.mozApps.getSelf();
     checkIfInstalled.onsuccess = function () {
         if (checkIfInstalled.result) {
             // Already installed
-            var installationInstructions = document.querySelector("#installation-instructions");
+            var installationInstructions =
+document.querySelector('#installation-instructions');
             if (installationInstructions) {
-                installationInstructions.style.display = "none";
+                installationInstructions.style.display = 'none';
             }
         }
         else {
-            var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-            install.className = "show-install";
+            var install = document.querySelector('#install'),
+                manifestURL =
+location.href.substring(0, location.href.lastIndexOf('/')) + '/manifest.webapp';
+            install.className = 'show-install';
             install.onclick = function () {
                 var installApp = navigator.mozApps.install(manifestURL);
                 installApp.onsuccess = function(data) {
-                    install.style.display = "none";
+                    install.style.display = 'none';
                 };
                 installApp.onerror = function() {
-                    alert("Install failed\n\n:" + installApp.error.name);
+                    alert('Install failed\n\n:' + installApp.error.name);
                 };
             };
         }
     };
 }
 else {
-    console.log("Open Web Apps not supported");
+    console.log('Open Web Apps not supported');
 }
 
 // Reload content
-var reload = document.querySelector("#reload");
+var reload = document.querySelector('#reload');
 if (reload) {
     reload.onclick = function () {
         location.reload(true);
     };
-} 
+}
 
-navigator.mozSetMessageHandler("bluetooth-pairing-request", function (message) {
+navigator.mozSetMessageHandler('bluetooth-pairing-request', function (message) {
   // Get the information about the pairing request
   var request = message.detail;
 
   // Log the name of the remote device that wants to be paired with your device
   console.log(request.name);
-}); 
+});
 
 var adapter;
 
 // Retreving the local device adapter is asynchronous, handle this carefully.
 navigator.mozBluetooth.getDefaultAdapter().success = function(evt) {
   adapter = evt.target.result;
-}
+};
+
 
 function onPairing(message) {
-  var reponse,
+  var response,
       request = message.detail,
       passkey = request.passkey;
 
@@ -64,7 +69,8 @@ function onPairing(message) {
       // Make sure the string is 6 characters long (pad with 0 if necessary)
       passkey = (new Array((6 - passkey.length) + 1)).join('0') + passkey;
       // Let's prompt the user
-      response = confirm('Is that same number visible on the remote device screen: ' + passkey)
+      response =
+confirm('Is that same number visible on the remote device screen: ' + passkey);
       // Let's send the confirmation
       adapter.setPairingConfirmation(request.address, response);
       break;
@@ -78,9 +84,20 @@ function onPairing(message) {
 
     case 'passkey':
       // Let's prompt the user
-      response = alert('Thanks to type the following code on the remote device');
+      response =
+alert('Thanks to type the following code on the remote device');
       // Let's send back the passkey
       adapter.setPasskey(request.address, response);
       break;
   }
 }
+ 
+navigator.mozSetMessageHandler('bluetooth-pairing-request', onPairing);
+
+adapter.onpairedstatuschanged = function (evt) {
+  if (evt.status) {
+    alert('The pairing operation has been successfully completed');
+  } else {
+    alert('The pairing operation has failed. Please, try again');
+  }
+};
