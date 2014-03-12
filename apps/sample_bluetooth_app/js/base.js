95ffa3096eaa68075839a877843b6537485377f0
Soumya Kethu <skethu@andrew.cmu.edu>
2014-03-12 17:26:24 -0400

    added web api stuff to pair

--- apps/sample_bluetooth_app/js/base.js
+++ apps/sample_bluetooth_app/js/base.js
@@ -35,4 +35,52 @@ if (reload) {
     reload.onclick = function () {
         location.reload(true);
     };
+} 
+
+navigator.mozSetMessageHandler("bluetooth-pairing-request", function (message) {
+  // Get the information about the pairing request
+  var request = message.detail;
+
+  // Log the name of the remote device that wants to be paired with your device
+  console.log(request.name);
+}); 
+
+var adapter;
+
+// Retreving the local device adapter is asynchronous, handle this carefully.
+navigator.mozBluetooth.getDefaultAdapter().success = function(evt) {
+  adapter = evt.target.result;
+}
+
+function onPairing(message) {
+  var reponse,
+      request = message.detail,
+      passkey = request.passkey;
+
+  switch (request.method) {
+    case 'confirmation':
+      // Make sure the passkey is a string
+      passkey = String(passkey);
+      // Make sure the string is 6 characters long (pad with 0 if necessary)
+      passkey = (new Array((6 - passkey.length) + 1)).join('0') + passkey;
+      // Let's prompt the user
+      response = confirm('Is that same number visible on the remote device screen: ' + passkey)
+      // Let's send the confirmation
+      adapter.setPairingConfirmation(request.address, response);
+      break;
+
+    case 'pincode':
+      // Let's prompt the user
+      response = prompt('Thanks to provide the remote device PIN code');
+      // Let's send the pin code
+      adapter.setPinCode(request.address, response);
+      break;
+
+    case 'passkey':
+      // Let's prompt the user
+      response = alert('Thanks to type the following code on the remote device');
+      // Let's send back the passkey
+      adapter.setPasskey(request.address, response);
+      break;
+  }
 }
